06a6f5dfa0d72829076dc2965586af552f462149
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-04-13 12:27:20 -0300

    Resolve requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
-
+    results = []
+    for file in instance.data:
+        result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word.upper() in line.upper():
+                result["ocorrencias"].append({"linha": i + 1})
+        if result["ocorrencias"]:
+            results.append(result)
+    return results
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
