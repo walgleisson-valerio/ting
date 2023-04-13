32d597fa556b52cde883dc5e06d7dee523416007
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-04-13 01:29:31 -0300

    Resolve requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for file in instance.data:
+        if path_file == file["nome_do_arquivo"]:
+            return None
+    txt_imported = txt_importer(path_file)
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_imported),
+        "linhas_do_arquivo": txt_imported
+    }
+    instance.enqueue(dict)
+    print(dict)
 
 
 def remove(instance):
